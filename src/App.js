0db0732455725462323ad17e3f019f8f54626d12
SoniaKarungi <98121137+SoniaKarungi@users.noreply.github.com>
2022-03-29 19:00:54 +0300

    fix linting errors

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   const [movies, setMovies] = useState([]);
 
   const getMoviesRequest = async () => {
-    const url = `http://www.omdbapi.com/?s=star wars&apikey=263d22d8`;
+    const url = 'http://www.omdbapi.com/?s=star wars&apikey=263d22d8';
     const response = await fetch(url);
     const responseJson = await response.json();
 
@@ -21,10 +21,9 @@ const App = () => {
     getMoviesRequest();
   }, []);
 
-
   return (
-    <div className='container-fluid movie-app'>
-      <div className='row'>
+    <div className="container-fluid movie-app">
+      <div className="row">
         <MovieList movies={movies} />
       </div>
     </div>
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const MovieList = (props) => {
-  return (
-    <>
-      {
-        props.movies.map((movie, index) => (
-          <div className='image-container d-flex justify-content-start m-3'>
-            <img src={movie.Poster} alt={movie.Title}></img>
+const MovieList = ({ movies }) => (
+  <>
+    {
+        movies.map((movie) => (
+          <div className="image-container d-flex justify-content-start m-3" key={movie.imdbID}>
+            <img src={movie.Poster} alt={movie.Title} />
           </div>
         ))
-      
+
       }
-    </>
-  );
+  </>
+);
+
+MovieList.propTypes = {
+  movies: PropTypes.string.isRequired,
 };
 
 export default MovieList;
